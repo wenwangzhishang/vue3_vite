366f721176368c7bc5efe4e90868244ba384e100
Jack Lin <wy_huan_lin@shimaowy.com>
2021-06-28 14:05:47 +0800

    readme

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
 
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
+      Vite Documentation111111
     </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
